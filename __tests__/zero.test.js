ac53aeda0fd475dc6a3b26cd41da004d045c21d1
lukehedger <luke.hedger@gmail.com>
2019-03-20 16:56:13 +0000

    Add taiko and functional test

--- __tests__/zero.test.js
+++ __tests__/zero.test.js
@@ -1,3 +1,15 @@
-test("Example Test", () => {
-  expect(true).toEqual(true);
+import { closeBrowser, goto, openBrowser, text } from "taiko";
+
+beforeAll(async () => {
+  await openBrowser()
+})
+
+afterAll(async () => {
+  await closeBrowser()
+})
+
+test("Zero App render", async () => {
+  await goto("http://localhost:5000")
+
+  expect(await text("Hello Zero").exists()).toBeTruthy();
 });
